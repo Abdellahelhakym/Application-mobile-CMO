257d237eb0de994b9bba0bbb43332b7892bd932e
Abdellahelhakym <abdellahelhakym00@gmail.com>
2026-05-20 09:53:35 -0700

    update

--- Back-end/db.js
+++ Back-end/db.js
@@ -1,19 +1,31 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
-db.connect((err) => {
+// Test connexion
+pool.getConnection((err, connection) => {
+
   if (err) {
-    console.error('Erreur de connexion à MySQL:', err);
-  } else {
-    console.log('Connecté à MySQL avec succès');
+    console.error('Erreur MySQL :', err);
+    return;
   }
+
+  console.log('Pool MySQL connecté');
+
+  connection.release();
 });
 
-module.exports = db;
+module.exports = pool;
